f69a609d5c6766a2a3d693842ef624bad1369165
Dmitri Pisarev <dimaip@gmail.com>
2016-07-16 16:42:58 +0300

    Refactor stickey nav

--- Packages/Sites/Sfi.Site/Frontend/js/_stickInParent.js
+++ Packages/Sites/Sfi.Site/Frontend/js/_stickInParent.js
@@ -1,30 +1,34 @@
-function stickInParent () {
-	var nav = document.querySelector('.js-stickInParent');
-	var parent = nav.parentElement;
+function stickInParent (selector) {
+	var fixedClass = 'isFixed';
+	selector = selector || '.js-stickInParent';
 
-	debouncedScroll(setStickyClass);
-
-	function setStickyClass(scrollPosition) {
-		if (scrollPosition > parent.offsetTop) {
-			nav.classList.add('isFixed');
+	this.nav = document.querySelector(selector);
+	if (!this.nav) {
+		return;
+	}
+	this.parent = this.nav.parentElement;
+	this.update = function (scrollPosition) {
+		if (scrollPosition > this.parent.offsetTop) {
+			this.nav.classList.add(fixedClass);
 		} else {
-			nav.classList.remove('isFixed');
+			this.nav.classList.remove(fixedClass);
 		}
-	}
+	};
+	debouncedScroll(this.update.bind(this));
+}
 
-	function debouncedScroll(callback) {
-		var lastKnownScrollPosition = 0;
-		var isTicking = false;
+function debouncedScroll(callback) {
+	var lastKnownScrollPosition = 0;
+	var isTicking = false;
 
-		window.addEventListener('scroll', function(e) {
-			lastKnownScrollPosition = window.scrollY;
-			if (!isTicking) {
-				window.requestAnimationFrame(function() {
-					setStickyClass(lastKnownScrollPosition);
-					isTicking = false;
-				});
-			}
-			isTicking = true;
-		});
-	}
+	window.addEventListener('scroll', function(e) {
+		lastKnownScrollPosition = window.scrollY;
+		if (!isTicking) {
+			window.requestAnimationFrame(function() {
+				callback(lastKnownScrollPosition);
+				isTicking = false;
+			});
+		}
+		isTicking = true;
+	});
 }
